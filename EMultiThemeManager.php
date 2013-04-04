d30e17ac01e247236057390ec1ec1beca89e756c
Tobias Munk <schmunk@usrbin.de>
2013-04-04 19:39:19 +0300

    Create EMultiThemeManager.php

---
+++ EMultiThemeManager.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Class file.
+ * @author    Tobias Munk <schmunk@usrbin.de>
+ * @link      http://www.phundament.com/
+ * @copyright Copyright &copy; 2005-2013 diemeisterei GmbH
+ * @license   http://www.phundament.com/license/
+ */
+
+/**
+ * Yii Framework ThemeManager class, switch themes by regular expressions for routes
+ *
+ * Usage:
+ *
+ *      'themeManager' => array(
+ *          'class' => 'EMultiThemeManager',
+ *          'rules' => array(
+ *              '^user/(.*)' => 'backend' // switches to 'backend' for all routes beginning with 'user/'
+ *          )
+ *       ),
+ *
+ * Note: The first matching rule wins.
+ *
+ * @author Tobias Munk <schmunk@usrbin.de>
+ */
+class EMultiThemeManager extends CThemeManager
+{
+    public $rules = array();
+
+    public function getTheme($name)
+    {
+        $controller = Yii::app()->controller;
+
+        foreach ($this->rules as $rule => $themeName) {
+            if (preg_match("|" . $rule . "|", $controller->route)) {
+                $name = $themeName;
+                break;
+            }
+        }
+
+        return parent::getTheme($name);
+    }
+}
